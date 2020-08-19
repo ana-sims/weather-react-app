461df57f94ad66cdde3081918a6b2fed01f5a70d
ana-sims <30598011+ana-sims@users.noreply.github.com>
2020-08-19 12:27:48 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Put stuff here</div>;
+  return (
+    <div className="App">
+      <div className="container">Put stuff here</div>
+    </div>
+  );
 }
 
 export default App;
